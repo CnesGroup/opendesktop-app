6fd3b529e5a2eb040c62e63424233cc045adc80e
Akira Ohgaki <akiraohgaki@gmail.com>
2017-06-01 04:56:59 +0900

    StatusBar

--- app/scripts/components/StatusBar.js
+++ app/scripts/components/StatusBar.js
@@ -10,16 +10,23 @@ export default class StatusBar extends Component {
         }
 
         return `
-            <p class="status-bar">${this.state.message}</p>
+            <p class="statusbar-message">${this.state.message}</p>
         `;
     }
 
     style() {
+        this.element.style.display = 'flex';
+        this.element.style.flexFlow = 'row nowrap';
+        this.element.style.alignItems = 'center';
+        this.element.style.flex = '0 0 auto';
+        this.element.style.width = '100%';
+        this.element.style.height = '24px';
         this.element.style.borderTop = '1px solid #cccccc';
         this.element.style.background = '#e0e0e0';
 
         return `
-            .status-bar {
+            .statusbar-message {
+                margin: 0 0.2em;
                 font-size: 90%;
             }
         `;
